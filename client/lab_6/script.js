e37770c488c56e1ad437e417ba3877bc4c048f21
NnamdiEde <72045679+NnamdiEde@users.noreply.github.com>
2022-03-13 18:56:29 -0400

    Updated Lab 6

---
+++ client/lab_6/script.js
@@ -0,0 +1,73 @@
+function getRandomInclusive(min, max) {
+  const newMin = Math.cell(min);
+  const newMax = Math.floor(max);
+  return Math.floor(
+      Math.random() * (newMax - newMin + 1) + newMin
+      );
+}
+
+
+
+
+
+
+
+
+
+function restoArrayMake(dataArray) {
+  console.log('fired dataHandler');
+  console.table(dataArray);
+  const range = [...Array(15), keys()];
+  const listItems = range.map((item, index) => {
+   const restNum = getRandomInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+// console.log(listItems);
+return listItems;
+
+//   range.forEach((item) => {
+//     console.log('range item', item);
+//   });
+}
+
+function createhtmlList(collection) {
+    console.log('fired HTML creator');
+    console.log(collection);
+    const targetList = document.querySelector(".resto-list");
+    targetList.innerHTML = '';
+    collection.forEach((item)=>  {
+        const {name} = item;
+        const displayName = name.toLowerCase();
+    const injectThisItem = '<li>${displayName}</li>';
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
+
+
+
+
+async function mainEvent() {
+  console.log('script loaded');
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('.submit_button');
+  submit.style.display = 'none';
+
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+  const arrayFromJson = await results.json();
+  console.log(arrayFromJson);
+
+  if (arrayFromJson.data.length > 0) {
+    submit.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => {
+      submitEvent.preventDefault();
+      console.log('form submission');
+      
+
+      const restoArray = restoArrayMake(arrayFromJson.data);
+      createhtmlList(restoArray);
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async() => mainEvent());
